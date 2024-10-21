d8d8504f3f0fc24b146097cd9601f2f425fe6e82
torano <fastrivertiger@gmail.com>
2024-10-21 18:57:38 +0900

    feat: add plugin

---
+++ Plugins/GameplayAbilitySystemExtension/Source/GameplayAbilitySystemExtension/Private/GameplayAbilitySystemExtensionBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GameplayAbilitySystemExtensionBPLibrary.h"
+#include "GameplayAbilitySystemExtension.h"
+
+UGameplayAbilitySystemExtensionBPLibrary::UGameplayAbilitySystemExtensionBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UGameplayAbilitySystemExtensionBPLibrary::GameplayAbilitySystemExtensionSampleFunction(float Param)
+{
+	return -1;
+}
+
